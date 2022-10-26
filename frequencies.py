4779fb6e6b680b7b07b5d8792c005d07cfaa7f58
shazeen123 <shazeenshaheen2003@gmail.com>
2022-10-26 16:08:09 +0100

    Solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,22 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    if len(items)!=0:
+        list = []
+        for value in items:
+            y = str(value)
+            list.append(y)
+        list.sort()
+        x = list[0]
+        i = 1
+        count = 1
+        while i < len(list):
+            if x!=list[i]:
+                frequencies[x]=count
+                count=1
+            elif x==list[i]:
+                count += 1
+            x=list[i]
+            i +=1
+        else:
+            frequencies[x]=count
     return frequencies
